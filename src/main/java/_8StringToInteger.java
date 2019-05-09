362f6b71ba2fb40a7e3ca4d34135b663df2b2b51
sakam0to <jingjiezhu@hotmail.com>
2019-05-09 16:11:24 +0800

    8 & 9

--- src/main/java/_8StringToInteger.java
+++ src/main/java/_8StringToInteger.java
@@ -1,66 +1,1 @@
-﻿/**
- * @author zhujingjie
- * @project LeetCode
- * @description 8 Perl就是使用这种方式把字符串转换成Int型数的
- * @date 2019-03-06 11:27
- */
-public class _8StringToInteger {
-    public int myAtoi(String str) {
-        /**
-         * 去除whitespace
-         */
-        str = str.trim();
-        /**
-         * 转成char[]
-         */
-        char[] input = str.toCharArray();
-
-        StringBuffer result = new StringBuffer();
-
-        /**
-         * 判断首位是否为正负号
-         */
-        int i = 0;
-        if(input.length > 0 && (input[i] == '-' || input[i] == '+')){
-            result.append(input[i++]);
-        }
-        /**
-         * 遍历直到遇见非数字字符
-         */
-        for(; i < input.length; i++){
-            int each = Character.digit(input[i], 10);
-            if(each < 0){
-                break;
-            }
-            result.append(each);
-        }
-
-        /**
-         * 转换，异常只有两种情况：越界、空字符串或正负号。
-         */
-        try{
-            return Integer.valueOf(result.toString());
-        } catch (Exception ex){
-            if(result.toString().equals("")|| result.toString().equals("+") || result.toString().equals("-")) {
-                return 0;
-            }else if(result.toString().startsWith("-")){
-                return Integer.MIN_VALUE;
-            }else {
-                return Integer.MAX_VALUE;
-            }
-        }
-    }
-
-    public static void main(String[] args){
-        _8StringToInteger test = new _8StringToInteger();
-        System.out.println(test.myAtoi("+02c9"));
-        System.out.println(test.myAtoi(""));
-        System.out.println(test.myAtoi("42"));
-        System.out.println(test.myAtoi("   -42"));
-        System.out.println(test.myAtoi("4193 with words"));
-        System.out.println(test.myAtoi("words and 987"));
-        System.out.println(test.myAtoi("       "));
-        System.out.println(test.myAtoi("       +"));
-        System.out.println(test.myAtoi("    -   "));
-    }
-}
+/** * @author zhujingjie * @project LeetCode * @description 8 Perl就是使用这种方式把字符串转换成Int型数的 * @date 2019-03-06 11:27 */public class _8StringToInteger {    public static void main(String[] args) {        _8StringToInteger test = new _8StringToInteger();        System.out.println(test.myAtoi("+02c9"));        System.out.println(test.myAtoi(""));        System.out.println(test.myAtoi("42"));        System.out.println(test.myAtoi("   -42"));        System.out.println(test.myAtoi("4193 with words"));        System.out.println(test.myAtoi("words and 987"));        System.out.println(test.myAtoi("       "));        System.out.println(test.myAtoi("       +"));        System.out.println(test.myAtoi("    -   "));    }    public int myAtoi(String str) {        /**         * 去除whitespace         */        str = str.trim();        /**         * 转成char[]         */        char[] input = str.toCharArray();        StringBuffer result = new StringBuffer();        /**         * 判断首位是否为正负号         */        int i = 0;        if (input.length > 0 && (input[i] == '-' || input[i] == '+')) {            result.append(input[i++]);        }        /**         * 遍历直到遇见非数字字符         */        for (; i < input.length; i++) {            int each = Character.digit(input[i], 10);            if (each < 0) {                break;            }            result.append(each);        }        /**         * 转换，异常只有两种情况：越界、空字符串或正负号。         */        try {            return Integer.valueOf(result.toString());        } catch (Exception ex) {            if (result.toString().equals("") || result.toString().equals("+") || result.toString().equals("-")) {                return 0;            } else if (result.toString().startsWith("-")) {                return Integer.MIN_VALUE;            } else {                return Integer.MAX_VALUE;            }        }    }}
---
+++ src/main/java/_9PalindromeNumber.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhujingjie
+ * @project LeetCode
+ * @description 9 判断一个数字是否是回文，不用转字符串的方法
+ * @date 2019-03-06 11:27
+ */
+public class _9PalindromeNumber {
+    //用除10取余的方法依次获取每位数，存入一个列表中，然后在比较对应位置是否相等
+    public boolean isPalindrome(int x) {
+        if(x < 0)
+            //负数不是回文
+            return false;
+
+        List<Integer> arrayList = new ArrayList<Integer>();
+        do{
+            int residue = x % 10;
+            arrayList.add(residue);
+            x = x / 10;
+        }while(x != 0);
+
+        for(int i = 0; i < arrayList.size()/2 ; i++){
+            int j = arrayList.size() - 1 - i;
+            if(!arrayList.get(i).equals(arrayList.get(j)))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        _9PalindromeNumber test = new _9PalindromeNumber();
+        System.out.println(test.isPalindrome(123));
+        System.out.println(test.isPalindrome(-123));
+        System.out.println(test.isPalindrome(121));
+        System.out.println(test.isPalindrome(-121));
+        System.out.println(test.isPalindrome(10));
+    }
+}
